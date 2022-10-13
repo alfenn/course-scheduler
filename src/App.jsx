f01b38a77f0db13a01cfda0fe0866c49214e5014
alfenn <52635707+alfenn@users.noreply.github.com>
2022-10-13 00:43:45 -0500

    fetch data from api

--- src/App.jsx
+++ src/App.jsx
@@ -3,49 +3,33 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 import Banner from './components/Banner';
 import CourseList from './components/CourseList'
 import 'bootstrap-icons/font/bootstrap-icons.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { useJsonQuery } from './utilities/fetch';
 
-const schedule = {
-  "title": "CS Courses for 2018-2019",
-  "courses": {
-    "F101" : {
-      "term": "Fall",
-      "number": "101",
-      "meets" : "MWF 11:00-11:50",
-      "title" : "Computer Science: Concepts, Philosophy, and Connections"
-    },
-    "F110" : {
-      "term": "Fall",
-      "number": "110",
-      "meets" : "MWF 10:00-10:50",
-      "title" : "Intro Programming for non-majors"
-    },
-    "S313" : {
-      "term": "Spring",
-      "number": "313",
-      "meets" : "TuTh 15:30-16:50",
-      "title" : "Tangible Interaction Design and Learning"
-    },
-    "S314" : {
-      "term": "Spring",
-      "number": "314",
-      "meets" : "TuTh 9:30-10:50",
-      "title" : "Tech & Human Interaction"
-    }
-  }
-};
-
-const App = () => {
-  const today = new Date();
-  const day = today.toLocaleString([], {weekday: 'long'});
-  const date = today.toLocaleDateString([], {dateStyle: 'long'})
-
+const Main = () => {
+  const [data, isLoading, error] = useJsonQuery('https://courses.cs.northwestern.edu/394/guides/data/cs-courses.php');
+  
+  if (error) return <h1>Error loading user data: {`${error}`}</h1>;
+  if (isLoading) return <h1>Loading user data...</h1>;
+  if (!data) return <h1>No user data found</h1>;
   return (
     <div className="container">
-      {/* <h1>Sample React code</h1>
+        {/* <h1>Sample React code</h1>
       <p>Today is {day}, {date}.</p> */}
-      <Banner title={schedule.title} />
-      <CourseList courses={schedule.courses} />
-    </div>
+        <Banner title={data.title} />
+        <CourseList courses={data.courses} />
+    </div>    
+  )    
+}
+
+
+const App = () => {
+  const queryClient = new QueryClient();  
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Main />
+    </QueryClientProvider>
   );
 };
 
